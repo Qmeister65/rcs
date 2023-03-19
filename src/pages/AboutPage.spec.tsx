99c8636fb7bb1ac1b846d01760347369a8eba703
Aleksandr S <rexsanya1999@gmail.com>
2023-03-19 18:13:15 +0300

    feat: add testing

---
+++ src/pages/AboutPage.spec.tsx
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import AboutPage from 'pages/AboutPage';
+import { render, screen } from '@testing-library/react';
+
+describe('About page', () => {
+  it('Render About page', () => {
+    render(<AboutPage />);
+    expect(screen.getByText(/project/i)).toBeInTheDocument();
+  });
+});
---
+++ src/pages/MainPage.spec.tsx
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import MainPage from 'pages/MainPage';
+import { render, screen } from '@testing-library/react';
+
+describe('Main page', () => {
+  it('render main page', () => {
+    const { container } = render(<MainPage />);
+    expect(screen.getByPlaceholderText(/SearchBar/i)).toBeInTheDocument();
+    expect(container.getElementsByClassName('cardsList')[0]).toBeInTheDocument();
+  });
+});
